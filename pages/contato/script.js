fa8d0544f1f3a47c307241cd2d41ef1712d3d836
_crhis_ <crhisnokia@live.com>
2023-01-21 20:30:03 -0300

    feat: validação de input nome e email feitas, não está reconhecendo o input telefone, precisa ser arrumado ainda

--- pages/contato/script.js
+++ pages/contato/script.js
@@ -1,52 +1,74 @@
 let inputs = document.querySelectorAll("input");
 let inputsHelper = document.querySelectorAll(".helper-text")
 
-console.log(inputs)
-console.log(inputsHelper)
+const inputValues = {
+    name: "input-name",
+    email: "input-email",
+    phone: "input-phone"
+}
 
+const inputsHelperMessages = {
+    name: "Seu username precisa ter 3 ou mais caracteres",
+    email: "Precisa inserir um email válido e que contenha '@' e '.com'",
+    phone: "Seu número de telefone precisa ter 9 ou mais números"
+
+}
+
+const events = ["blur", "change"]
 
 function estilizarInputCorreto(input, helper) {
     helper.classList.remove("visible");
     input.classList.remove("error");
     input.classList.add("correct");
-  }
-  
+}
+
 function estilizarInputIncorreto(input, helper) {
     helper.classList.add("visible");
     input.classList.add("error");
     input.classList.remove("correct");
-  }
-
-
+}
 
 for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener("blur", (e)=>{
-        let valor = e.target.value
-        if(!valor){
-            inputsHelper[i].innerText = "Seu username precisa ter 3 ou mais caracteres";
+    inputs[i].addEventListener(events[i], (e) => {
+
+        let inputValue = e.target.value
+
+        if (!inputValue || inputValue.length < 3 && inputs[i].id === inputValues.name) {
+            inputsHelper[i].innerText = inputsHelperMessages.name
             estilizarInputIncorreto(inputs[i], inputsHelper[i])
-        }else{
+        } else {
             estilizarInputCorreto(inputs[i], inputsHelper[i]);
 
         }
-    })
+
+        if (inputs[i].id === inputValues.email) {
     
-}
+            if (inputValue.includes("@") && inputValue.includes(".com") && !inputValue.includes("@.com")) {
+                estilizarInputCorreto(inputs[i], inputsHelper[i]);
 
+            } else {
+                inputsHelper[i].innerText = inputsHelperMessages.email
+                estilizarInputIncorreto(inputs[i], inputsHelper[i])
 
-inputs.forEach(input => {
-
-    input.addEventListener("blur", (e)=>{
-        let valor = e.target.value;
-
-        if(!valor){
-            inputsHelper.innerText = "Seu username precisa ter 3 ou mais caracteres";
-            estilizarInputIncorreto(input, inputsHelper)
-        }else{
-            estilizarInputCorreto(input, inputsHelper);
+            }
 
         }
-        
-    })
+
+        if (inputs[i].id === inputValues.phone) {
     
-});
+            if (inputValue.length < 9) {
+                inputsHelper[i].innerText = inputsHelperMessages.phone
+                estilizarInputIncorreto(inputs[i], inputsHelper[i])
+            } else {
+                estilizarInputCorreto(inputs[i], inputsHelper[i]);
+              
+
+            }
+
+        }
+
+
+    })
+
+}
+
