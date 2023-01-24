199fdf6745cff82025e33752242292219d0e1f24
_crhis_ <crhisnokia@live.com>
2023-01-24 14:59:19 -0300

    feat: criacao de botao para falha no envio dos dados, falta terminar ainda

---
+++ pages/login/script.js
@@ -0,0 +1,20 @@
+// Get the modal
+var modal = document.getElementById("my-modal");
+
+var btn = document.getElementById("my-btn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
