33eba8e98a86fe015e0ad32a24761645145ba1e6
_crhis_ <crhisnokia@live.com>
2023-01-27 10:53:12 -0300

    refactor: refatorando modal em javascript para validacoes de input

--- pages/login/script.js
+++ pages/login/script.js
@@ -21,14 +21,15 @@ togglePassword.addEventListener("click", function () {
 
 // functions of input validations
 
-function estilizarInputCorreto(input, span) {
+function styleInputCorrect(input, span) {
 
   if (span.id === spanValues.email) {
     span.innerText = ""
-    openModal(input.value)
+    openModal(input)
   }
   if (span.id === spanValues.password) {
     span.innerText = ""
+    openModal(input)
   }
 
   input.classList.remove("error");
@@ -36,7 +37,7 @@ function estilizarInputCorreto(input, span) {
   togglePassword.style.color = "#fff"
 }
 
-function estilizarInputIncorreto(input, span) {
+function styleInputIncorrect(input, span) {
   if (span.id === spanValues.email) {
     span.innerText = "O campo e-mail não pode ser vazio"
   }
@@ -49,11 +50,14 @@ function estilizarInputIncorreto(input, span) {
 }
 
 // Abrir e Fechar modal
-var modal = document.getElementById("my-modal");
+let modal = document.getElementById("my-modal");
 
-var btn = document.getElementById("my-btn");
+let btn = document.getElementById("my-btn");
 
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
+
+let textModal = document.querySelector("p");
+
 
 btn.onclick = function () {
   modal.style.display = "block";
@@ -64,12 +68,23 @@ span.onclick = function () {
 }
 
 function openModal(input) {
-  if (!input.includes("@") && !input.includes(".com")) {
-    modal.style.display = "block";
-  } else {
-    modal.style.display = "none";
+  if (input.id === "email") {
+    if (!input.value.includes("@") && !input.value.includes(".com")) {
+      modal.style.display = "block";
+      textModal.innerText = "Email deve conter '@' e também '.com' entre seus caracteres"
+    } else {
+      modal.style.display = "none";
+    }
   }
+  if (input.id === "password") {
+    if(input.value.length < 8){
+      modal.style.display = "block";
+      textModal.innerText = "Password deve conter mais que 8 caracteres"
+    }else{
+      modal.style.display = "none";
 
+    }
+  }
 }
 
 // Validar Inputs
@@ -92,10 +107,16 @@ for (let i = 0; i < inputs.length; i++) {
 
     if (!value) {
 
-      estilizarInputIncorreto(input, spans[i])
+      styleInputIncorrect(input, spans[i])
     } else {
 
-      estilizarInputCorreto(input, spans[i])
+      styleInputCorrect(input, spans[i])
+    }
+
+    if (value) {
+      if (value.length < 8) {
+
+      }
     }
 
 
