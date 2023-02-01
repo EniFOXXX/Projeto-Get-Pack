eaf9f70f6c99789a91344f9ce2092d953530b63a
Enivaldo_Souza <enif16@gmail.com>
2023-02-01 00:09:50 -0300

    Alterações no menu

--- pages/inicio/script.js
+++ pages/inicio/script.js
@@ -1,39 +1,57 @@
-let nav = document.querySelectorAll(".nav a");
-let y =0
-function altcor(){
-for(let a=0;a<nav.length;a++){
-    nav[a].style.backgroundColor = "rgb(88, 143, 220)"
-    nav[a].style.borderRadius = "10px"
-    nav[a].style.padding = "5px"
-    nav[a].style.transition = "0.8s"
-    nav[a].style.color = "white"
-    nav[a].style.textAlign = "center"
-    nav[a].style.justifyContent = "center"
-    nav[a].style.height="25px";
-    nav[a].style.width="90px";
+    const nav = document.querySelectorAll(".nav");
     
-}
-}
+  
+   console.log(nav)
+   
+   /*nav.addEventListener("mouseover", function(){
+    nav.forEach(x => x.classList.toggle("acesso"))
+   })*/
+  
 
-function altcor1(){
-    for(let a=0;a<nav.length;a++){
-    nav[a].style.backgroundColor = ""
-    nav[a].style.borderRadius = ""
-    nav[a].style.padding = ""
-    nav[a].style.transition = ""
-    nav[a].style.color = ""
+  function mostrarMenu(){
+        
+        nav.forEach(x => x.classList.toggle("acesso")); 
+        
+        console.log(nav)
+        
     }
-
-}
-for(let i=0;i<nav.length;i++){
     
-    nav[i].addEventListener("mouseover",altcor) 
+  
     
-     console.log(nav[i])
-}
-
-
-for(let b=0;b<nav.length;b++){
-    nav[b].addEventListener("mouseout",altcor1);
+   function apagarMenu(){
     
+    nav.forEach(x => x.classList.toggle("acesso")); 
+    
+     
+      console.log(nav)
     }
+    
+    
+   
+   for(let i=0; i<nav.length;i++){
+    nav[i].addEventListener("mouseover",mostrarMenu);
+    
+    console.log(nav)
+   }
+    
+    
+   
+ 
+
+   for(let a=0; a<nav.length;a++){
+    nav[a].addEventListener("mouseout",apagarMenu);
+    
+    console.log(nav)
+   }
+    
+    
+    
+   
+  
+
+    
+
+     
+
+
+
