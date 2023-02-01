9ae88d4a80735b82105777c363cfbc09c57e01e5
Enivaldo_Souza <enif16@gmail.com>
2023-02-01 20:33:22 -0300

    Ativando mouseover no menu

--- pages/inicio/script.js
+++ pages/inicio/script.js
@@ -1,49 +1,32 @@
     const nav = document.querySelectorAll(".nav");
-    
-  
-   console.log(nav)
-   
-   /*nav.addEventListener("mouseover", function(){
-    nav.forEach(x => x.classList.toggle("acesso"))
-   })*/
+
   
 
-  function mostrarMenu(){
-        
-        nav.forEach(x => x.classList.toggle("acesso")); 
-        
-        console.log(nav)
-        
-    }
+   nav.forEach(function(nav){
     
-  
-    
-   function apagarMenu(){
-    
-    nav.forEach(x => x.classList.toggle("acesso")); 
-    
-     
+    nav.addEventListener("mouseover",()=>{
+      nav.classList.toggle("acesso"); 
       console.log(nav)
-    }
-    
-    
+    })
+   })
    
-   for(let i=0; i<nav.length;i++){
-    nav[i].addEventListener("mouseover",mostrarMenu);
-    
-    console.log(nav)
-   }
-    
-    
-   
- 
 
-   for(let a=0; a<nav.length;a++){
-    nav[a].addEventListener("mouseout",apagarMenu);
+   nav.forEach(function(nav){
     
-    console.log(nav)
-   }
-    
+    nav.addEventListener("mouseout",()=>{
+      nav.classList.toggle("acesso"); 
+      console.log(nav)
+    })
+
+   })
+   
+   
+   
+   
+   
+   
+   
+   
     
     
    
