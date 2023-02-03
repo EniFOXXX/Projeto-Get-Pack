bf071208d4c4fd4dd2b4d94f582bd16fdf278b5b
unknown <xandmaxis@gmail.com>
2023-02-02 22:43:55 -0300

    adicionando arquivo clientes.js

---
+++ pages/clientes/clientes.js
@@ -0,0 +1,19 @@
+console.log('teste arquivo clientes.js');
+
+let logo1 = document.getElementById("logo1");
+
+// logo1.addEventListener("mouseover", logo1.style.border = "100px");
+logo1.addEventListener("mouseover", function(e){
+    console.log('mouseover ok');
+    // e.target.border = "1px"
+    e.target.border = "1px"
+});
+logo1.addEventListener("mouseleave", function(e){
+    console.log('mouseleave ok');
+    e.target.border = "0px"
+
+});
+
+// logo1.style.border = "100px";
+// logo1.style.borderColor = "blue";
+// console.log(logo1.style)
