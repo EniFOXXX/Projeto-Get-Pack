949fe65aa3e9c0bce9fc8939bfa9cdcc4f152638
_crhis_ <crhisnokia@live.com>
2023-02-11 19:48:53 -0300

    refactor: ajeitando detalhes

--- pages/contato/script.js
+++ pages/contato/script.js
@@ -3,7 +3,7 @@ let inputsHelper = document.querySelectorAll(".helper-text");
 let textArea = document.querySelector("textarea");
 let button = document.querySelector("button[type='submit']");
 
-const inputValues = {
+const inputObject = {
     name: "input-name",
     email: "input-email",
     phone: "input-phone"
@@ -16,15 +16,13 @@ const inputsHelperMessages = {
 
 }
 
-const events = "change"
-
-function estilizarInputCorreto(input, helper) {
+function styleInputCorrect(input, helper) {
     helper.classList.remove("visible");
     input.classList.remove("error");
     input.classList.add("correct");
 }
 
-function estilizarInputIncorreto(input, helper) {
+function styleInputIncorrect(input, helper) {
     helper.classList.add("visible");
     input.classList.add("error");
     input.classList.remove("correct");
@@ -34,66 +32,82 @@ function estilizarInputIncorreto(input, helper) {
 const inputLength = inputs.length;
 
 for (let i = 0; i < inputLength; i++) {
-    inputs[i].addEventListener(events, (e) => {
 
-        let inputValue = e.target.value
+    inputs[i].addEventListener("change", e => {
+        let inputValue = e.target.value;
+        switch (inputs[i].id) {
+            case inputObject.name:
+                if (!inputValue || inputValue.length < 3) {
+                    inputsHelper[i].innerText = inputsHelperMessages.name;
+                    styleInputIncorrect(inputs[i], inputsHelper[i])
+                } else {
+                    styleInputCorrect(inputs[i], inputsHelper[i])
+                }
+                break;
+            case inputObject.email:
+                if (!inputValue.includes("@") || !inputValue.includes(".com") || inputValue.includes("@.com")) {
+                    inputsHelper[i].innerText = inputsHelperMessages.email;
+                    styleInputIncorrect(inputs[i], inputsHelper[i]);
+                } else {
+                    styleInputCorrect(inputs[i], inputsHelper[i]);
+                }
+                break;
+            case inputObject.phone:
+                var regexPhone = /^\d{11,}$/;
 
-        if (!inputValue || inputValue.length < 3 && inputs[i].id === inputValues.name) {
-            inputsHelper[i].innerText = inputsHelperMessages.name
-            estilizarInputIncorreto(inputs[i], inputsHelper[i])
-        } else {
-            estilizarInputCorreto(inputs[i], inputsHelper[i]);
-
-        }
-
-        if (inputs[i].id === inputValues.email) {
-
-            if (inputValue.includes("@") && inputValue.includes(".com") && !inputValue.includes("@.com")) {
-                estilizarInputCorreto(inputs[i], inputsHelper[i]);
-
-            } else {
-                inputsHelper[i].innerText = inputsHelperMessages.email
-                estilizarInputIncorreto(inputs[i], inputsHelper[i])
-            }
-        }
-
-        if (inputs[i].id === inputValues.phone) {
-
-            var regexPhone = /[^0-9.]/;
-
-            if (inputValue.length < 11 || regexPhone.test(inputValue)) {
-                inputsHelper[i].innerText = inputsHelperMessages.phone
-                estilizarInputIncorreto(inputs[i], inputsHelper[i])
-            } else {
-                estilizarInputCorreto(inputs[i], inputsHelper[i]);
-            }
+                if (!regexPhone.test(inputValue)) {
+                    inputsHelper[i].innerText = inputsHelperMessages.phone;
+                    styleInputIncorrect(inputs[i], inputsHelper[i]);
+                } else {
+                    styleInputCorrect(inputs[i], inputsHelper[i]);
+                }
+                break;
         }
     })
+
 }
 
 // função para enviar o formulário após todas as validações
-button.addEventListener("click", (e)=>{
+
+function sendForm() {
+    // Example using Fetch API to submit form data to the server
+    fetch("/api/submit-form", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          window.alert("Formulário enviado com sucesso.");
+        } else {
+          window.alert("Ocorreu um erro ao enviar o formulário.");
+        }
+      })
+      .catch(error => {
+        console.error("Error submitting form:", error);
+        window.alert("Ocorreu um erro ao enviar o formulário.");
+      });
+  }
+
+  
+button.addEventListener("click", (e) => {
     e.preventDefault()
     let inputName = inputs[0].value;
     let inputEmail = inputs[1].value;
     let inputPhone = inputs[2].value;
     let textMessage = textArea.value;
 
-    if(inputName === ""){
-        window.alert("Seu nome precisa ser preenchido")
-    } 
-    else if(inputEmail === ""){
-        window.alert("Seu Email precisa ser preenchido")
+    if(!inputName || !inputEmail || !inputPhone || !textMessage ){
+        window.alert("Para enviar sua Mensagem todos os campos devem ser preenchidos")
+    }else{
+        // aqui iria a função de conexão com o banco de dados
+        window.alert("Formulário enviado com sucesso.");
+        sendForm()
     }
-   else if(inputPhone === ""){
-        window.alert("Seu Telefone precisa ser preenchido")
-    }
-    else if(textMessage === ""){
-        window.alert("Sua Mensagem precisa ser preenchido")
-    } else{
-        window.alert("Sua mensagem foi enviada com Sucesso")
-    }
-    
+
 })
 
 
